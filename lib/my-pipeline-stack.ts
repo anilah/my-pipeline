1042f9daf5ed580842d29e297540394d327ad75a
Anil kumar <anil_ah@yahoo.co.in>
2021-12-02 17:10:39 +1300

    Update my-pipeline-stack.ts
    update

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from '@aws-cdk/core';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as pipelines from '@aws-cdk/pipelines';
 import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
 
 export class MyPipelineStack extends cdk.Stack {
@@ -8,9 +10,30 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.gitHub('anilah/my-pipeline', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
+
+    /** 
+    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.connection('anilah/my-pipeline', 'main', {
+        connectionArn: 'arn:aws:codestar-connections:ap-southeast-2:323619686659:connection/1645fd72-c2a7-4c45-8fac-23695dc7806a', // Created using the AWS console * });',
+       }),
+      
+        commands: [ 'npm ci','npm run build', 'npx cdk synth']
+       // commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+        buildEnvironment: {
+          environmentVariables: {
+            'GITHUB_AUTH_TOKEN': {
+              
+              //value: `${githubSecretArn.stringValue}:pat`,
+              value : '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:pat',
+              type: codebuild.BuildEnvironmentVariableType.SECRETS_MANAGER
+            }
+          }
+          */
+     })
     });
   }
 }
